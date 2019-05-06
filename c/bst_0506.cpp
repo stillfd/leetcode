2054b7d32f24b391d6a3835a70e827ac190cb6f9
stillfd <stillfd@163.com>
2019-05-06 23:01:11 +0800

    revisit bst and different traversal methods

---
+++ c/bst_0506.cpp
@@ -0,0 +1,152 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stack>
+using namespace std;
+typedef struct BiTNode{
+	char val;
+	BiTNode* left;
+	BiTNode* right;
+}BiTNode, *BiTree;
+
+
+void create(BiTree *T)
+{
+	char ch;
+	ch = getchar();
+	if(ch == ' ')
+	{
+		*T = NULL;
+	}
+	else
+	{
+		*T = (BiTNode*)malloc(sizeof(BiTNode));
+		(*T)->val = ch;
+		create(&(*T)->left);
+		create(&(*T)->right);
+	}
+	return;
+}
+
+void preOrder1(BiTree T)
+{
+	if(T)
+	{
+		printf("%c", T->val);
+		preOrder1(T->left);
+		preOrder1(T->right);
+	}
+}
+void inOrder1(BiTree T)
+{
+	if(T)
+	{
+		inOrder1(T->left);
+		printf("%c", T->val);
+		inOrder1(T->right);
+	}
+}
+void postOrder1(BiTree T)
+{
+	if(T)
+	{
+		
+		postOrder1(T->left);
+		postOrder1(T->right);
+		printf("%c", T->val);
+	}
+}
+
+void preOrder(BiTree T)
+{
+	BiTree p;
+	p = T;
+	stack<BiTree> s;
+	while(1)
+	{
+		while(p)
+		{
+			printf("%c",p->val);
+			s.push(p);
+			p = p->left;
+		}
+		if(s.empty()) break;
+		p = s.top();
+		s.pop();
+		p = p->right;
+	}
+}
+void inOrder(BiTree T)
+{
+	BiTree p;
+	p = T;
+	stack<BiTree> s;
+	while(1)
+	{
+		while(p)
+		{
+			
+			s.push(p);
+			p = p->left;
+		}
+		if(s.empty()) break;
+		p = s.top();
+		printf("%c",p->val);
+		s.pop();
+		p = p->right;
+	}
+}
+
+void postOrder(BiTree T)
+{
+	BiTree p, prev;
+	p = T;
+	prev = NULL;
+	stack<BiTree> s;
+	while(1)
+	{
+		while(p)
+		{
+			s.push(p);
+			p = p->left;
+		}
+		if(s.empty()) break;
+		p = s.top();
+		if(p->right != NULL && p->right!=prev)
+		{
+			p = p->right;
+			prev = p;
+		}
+		else
+		{
+			prev = p;
+			printf("%c",p->val);
+			s.pop();	
+			p = NULL;
+		}
+	}
+}
+
+int main()
+{
+	BiTree T = NULL;
+	create(&T);
+	printf("Display BST, preorder, recursive\n");
+	preOrder1(T); 
+	printf("\n");
+	printf("Display BST, inorder, recursive\n");
+	inOrder1(T);
+	printf("\n");
+	printf("Display BST, postorder, recursive\n");
+	postOrder1(T);	
+	printf("\n");
+	printf("Display BST, preorder, iterative\n");
+	preOrder(T);
+	printf("\n");
+	printf("Display BST, inorder, iterative\n");
+	inOrder(T);
+	printf("\n");
+	printf("Display BST, postorder, iterative\n");
+	postOrder(T);
+	printf("\n");
+	return 0;
+}
